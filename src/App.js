b27826b90f742d5e07f6ff54bf3ca9ee0c50cab5
Camila Gallegos <camigm1@gmail.com>
2024-01-29 21:59:33 -0700

    adding more functionality

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ import BookList from "./components/BookList";
 function App() {
   const [books, setBooks] = useState([]);
 
+  const editBookById = (id, newTitle) => {
+    const updatedBooks = books.map((book) => {
+      if (book.id === id) {
+        return { ...book, title: newTitle };
+      }
+      return book;
+    });
+    setBooks(updatedBooks);
+  };
+
   const deleteBookById = (id) => {
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
@@ -22,7 +32,8 @@ function App() {
 
   return (
     <div className="app">
-      <BookList onDelete={deleteBookById} books={books} />
+      <h1>Reading List</h1>
+      <BookList onDelete={deleteBookById} books={books} onEdit={editBookById} />
       <BookCreate onCreate={createBook} />
     </div>
   );
--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -16,7 +16,7 @@ function BookCreate({ onCreate }) {
     <div className="book-create">
       <h3>Add a Book</h3>
       <form onSubmit={handleSubmit}>
-        {/* <label>Title</label> */}
+        <label>Title</label>
         <input className="input" value={title} onChange={handleChange} />
         <button className="button">Create!</button>
       </form>
